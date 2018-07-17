25c7ce4e3b73a1e78eb23e1af2d9f4b571be43a6
Kumail Naqvi <kumailmn@gmail.com>
2018-07-17 14:08:31 -0400

    Update .eslintrc, lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': 'off'
+    'max-len': 'off',
+    'no-unused-vars': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
