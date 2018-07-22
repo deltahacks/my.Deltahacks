ac76d62ff9a9bc73f676313fe6bfe5c2640c070c
Anant Jain <anant.j2409@gmail.com>
2018-07-22 23:25:50 +0530

    Rejection Page

--- src/components/Rejected.vue
+++ src/components/Rejected.vue
@@ -1,15 +1,26 @@
 <template>
-
-</template>
+<div class="wrapper">
+    <div class="modal">
+        <div class="modal-top">
+          <span class="logocss">
+            <img src="@/assets/logo.png" height="90" width="90" alt="DeltaHacks Logo" />
+          </span>
+          <br>
+            <img class="modal-icon" src="https://media.giphy.com/media/rAKIEXmwAeHHq/giphy.gif" alt="Trophy" />
+            <div class="modal-header">We're Sorry</div>
+            <div class="modal-subheader">Due to a high number of really competitive applications this year, we are extremely sorry to inform you that you have not been selected for DeltaHacks V<br><br> We wish you the best for your future endeavours.</div>
+        </div>
+    </div>
+</div>
+ </template>
 
 <script>
 export default {
-  name: 'Rejected',
   data() {
     return {};
   },
 };
 </script>
 
-<style scoped>
+<style scoped src="../assets/css/acceptreject.css">
 </style>
