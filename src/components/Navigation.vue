f18cb79771829ac32c80b132d401300d1c3f169e
Ghazi <salamga@mcmaster.ca>
2018-08-16 00:51:42 -0400

    optimized mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,45 @@
 <template>
   <v-toolbar dark class="toolbar">
-    <div class="text-xs-center, hide">
+
+    <v-spacer></v-spacer>
+
+    <a href="/" class="smaller delta"><img src="@/assets/logo.png" height=53px alt="DeltaHacks Logo"></a>
+    <v-toolbar-title id="title">
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    
+
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <!-- <v-btn>
+        <v-badge right color="red" overlap>
+          <v-icon slot="badge" dark small>fas fa-exclamation</v-icon>
+          <v-icon large color="grey lighten-1">
+            far fa-bell
+          </v-icon>
+        </v-badge>
+      </v-btn> -->
+      <v-btn flat to="/status" v-if="c_user" >Status</v-btn>
+      <v-btn flat to="/apply" v-if="c_user" >Apply Now</v-btn>
+      <v-btn flat to="/signup" v-if="!c_user">Signup</v-btn>
+      <v-btn flat to="/login" v-if="!c_user">Login</v-btn>
+      <v-btn flat to="/">Contact</v-btn>
+      <v-btn flat to="/">FAQ</v-btn>
+      <!-- <v-btn flat @click.prevent="logout" v-if="c_user">Logout</v-btn> -->
+    </v-toolbar-items>
+      <v-chip color="white" text-color="black" class="clickable, hide-chip" style="margin: 0px 40px">
+        <v-avatar><v-icon>account_circle</v-icon></v-avatar>
+        {{c_user.email}}
+      </v-chip>
+      <!-- <v-chip  class="clickable" @click.prevent="logout" v-if="c_user">
+        Logout
+      </v-chip> -->
+    <v-spacer></v-spacer>
+
+    <div class="text-xs-center, hide-nav">
       <v-menu offset-y>
-        <v-btn slot="activator" color="grey darken-3" dark open-on-hover="True">Menu</v-btn>
+        <v-btn slot="activator" color="grey darken-3" dark right>Menu</v-btn>
 
         <v-list>
           <v-list-tile to="/status">
@@ -24,39 +61,7 @@
         </v-list>
       </v-menu>
     </div>
-
-    <v-spacer></v-spacer>
-
-    <a href="/" class="smaller delta"><img src="@/assets/logo.png" height=53px alt="DeltaHacks Logo" /></a>
-    <v-toolbar-title id="title">
-    </v-toolbar-title>
-
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <!-- <v-btn>
-        <v-badge right color="red" overlap>
-          <v-icon slot="badge" dark small>fas fa-exclamation</v-icon>
-          <v-icon large color="grey lighten-1">
-            far fa-bell
-          </v-icon>
-        </v-badge>
-      </v-btn> -->
-      <v-btn flat to="/status" v-if="c_user" >Status</v-btn>
-      <v-btn flat to="/apply" v-if="c_user" >Apply Now</v-btn>
-      <v-btn flat to="/signup" v-if="!c_user">Signup</v-btn>
-      <v-btn flat to="/login" v-if="!c_user">Login</v-btn>
-      <v-btn flat to="/">Contact</v-btn>
-      <v-btn flat to="/">FAQ</v-btn>
-      <!-- <v-btn flat @click.prevent="logout" v-if="c_user">Logout</v-btn> -->
-    </v-toolbar-items>
-      <v-chip color="white" text-color="black" class="clickable" style="margin: 0px 40px">
-        <v-avatar><v-icon>account_circle</v-icon></v-avatar>
-        {{c_user.email}}
-      </v-chip>
-      <!-- <v-chip  class="clickable" @click.prevent="logout" v-if="c_user">
-        Logout
-      </v-chip> -->
-    <v-spacer></v-spacer>
+    
   </v-toolbar>
 </template>
 
@@ -118,8 +123,20 @@ export default {
   cursor: pointer;
 }
 @media only screen and (min-width: 960px) {
-  .hide {
+  .hide-nav {
   display: none;
+  }
+}
+
+@media only screen and (max-width: 959px) {
+  .hide-chip {
+  display: none;
+  }
+  .smaller.delta {
+  width: 2%;
+  height: auto;
+  position: absolute;
+  left: 10%;
   }
 }
 
