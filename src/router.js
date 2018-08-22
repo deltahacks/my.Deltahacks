0f2078a1f27c807c8d44381f43228fcf92184616
Kumail Naqvi <kumailmn@gmail.com>
2018-08-22 18:17:42 -0400

    Cleanup

--- src/router.js
+++ src/router.js
@@ -15,7 +15,6 @@ import v404 from './views/404.vue';
 import AdminSignup from './views/AdminSignup.vue';
 import Accepted from './components/Accepted.vue';
 import Rejected from './components/Rejected.vue';
-import Debug from './views/Debug.vue';
 
 Vue.use(Router);
 
@@ -59,11 +58,6 @@ const router = new Router({
       path: '/playground',
       name: 'Playground',
       component: Playground,
-    },
-    {
-      path: '/debug',
-      name: 'Debug',
-      component: Debug,
     },
     {
       path: '/404',
