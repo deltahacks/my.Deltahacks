73e5848120aed6c640a4f5beb47ad78e26526133
Anant Jain <anant.j2409@gmail.com>
2018-08-23 13:19:30 -0400

    Accepted, Rejected Pages

--- src/components/Rejected.vue
+++ src/components/Rejected.vue
@@ -6,9 +6,9 @@
             <img src="@/assets/logo.png" height="90" width="90" alt="DeltaHacks Logo" />
           </span>
           <br>
-            <img class="modal-icon" src="https://media.giphy.com/media/rAKIEXmwAeHHq/giphy.gif" alt="Trophy" />
+            <img class="modal-icon" src="https://media.giphy.com/media/rAKIEXmwAeHHq/giphy.gif" alt="Sad" />
             <div class="modal-header">We're Sorry</div>
-            <div class="modal-subheader">Due to a high number of really competitive applications this year, we are extremely sorry to inform you that you have not been selected for DeltaHacks V<br><br> We wish you the best for your future endeavours.</div>
+            <div class="modal-subheader">Due to a high number of applications this year, we are sorry to inform you that you have not been selected for DeltaHacks V.<br><br> We wish you the very best for your future endeavours.</div>
         </div>
     </div>
 </div>
