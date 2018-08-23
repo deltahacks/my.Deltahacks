2cb7759ef526b59483c160b0b56730c26c3d3e53
Ghazi <salamga@mcmaster.ca>
2018-08-23 19:39:06 -0400

    changed background pic

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app class="dashboard background" style="background-image: url('https://wallpapersite.com/images/pages/pic_w/14088.png');">
+    <v-app class="dashboard background back" >
     <Navbar2/>
     <div class='container-status100'>
         <div class='question-popout'>
@@ -92,10 +92,15 @@ overflow: hidden;
   height: 100%;
   top: 0;
   left: 0;
-  background-color: rgba(255,255,255,0.8);
+  background-color: rgba(255,255,255,0.2);
 } 
 .panel {
     background-color: rgba(255,255,255,0.8);
+}
+.back {
+background: #5C258D;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #4389A2, #5C258D);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #4389A2, #5C258D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
 </style>
