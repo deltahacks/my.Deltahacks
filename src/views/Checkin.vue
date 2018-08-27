9c83bd18cf1125509e36cc09473ba1abc19530b5
Kumail Naqvi <kumailmn@gmail.com>
2018-08-26 22:50:42 -0400

    Update layout

--- src/views/Checkin.vue
+++ src/views/Checkin.vue
@@ -1,17 +1,36 @@
 <template>
     <v-app>
+        <h2>{{$route.params.id}}</h2>
         <v-btn class="checkinButtons">Add meal</v-btn>
+        <v-btn @click="checkin" class="checkinButtons">Check in</v-btn>
     </v-app>
 </template>
 
 <script>
+import db from '../private/firebase_init';
 export default {
-  data() {
-    return {};
-  },
-  mounted() {
-    console.log(this.$route.params.id);
-  },
+    data() {
+        return { d: 'l' };
+    },
+    methods: {
+        checkin() {
+            db
+                .collection('hackathon')
+                .doc('DHV')
+                .collection('checked in')
+                .doc(this.$route.params.id.toLowerCase())
+                .set({
+                    checkedIn: true,
+                    time: new Date(),
+                    by: this.$store.state.firebase.auth().currentUser.email.toLowerCase(),
+                })
+                .then(() => console.log('Successfully written'))
+                .catch(err => console.log(err));
+        },
+    },
+    mounted() {
+        console.log(this.$route.params.id);
+    },
 };
 </script>
 
