901c24bb6ee461a58edbd6dcb2987b6f6c9a7361
Kumail Naqvi <kumailmn@gmail.com>
2018-08-26 20:14:04 -0400

    Add checkin component

---
+++ src/views/Checkin.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <v-btn class="checkinButtons">Add meal</v-btn>
+    </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(this.$route.params.id);
+  },
+};
+</script>
+
+<style scoped>
+.checkinButtons {
+    height: 10%;
+}
+</style>
