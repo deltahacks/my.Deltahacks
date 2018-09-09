f111b575cea51954861e004ac989d1c33a05c484
Kumail Naqvi <kumailmn@gmail.com>
2018-09-09 13:25:01 -0400

    Minor change

--- admin/src/components/DataTable.vue
+++ admin/src/components/DataTable.vue
@@ -35,186 +35,180 @@
       </template>
     </v-data-table>
     <div class="text-xs-center">
-      <v-pagination v-model="page" :length="this.numApplicants" circle @input="nextPage"></v-pagination>
+      <v-pagination v-model="page" :length="numApplicants" circle @input="nextPage"></v-pagination>
     </div>
   </v-card>
 
 </template>
 
 <script>
-import Vue from "vue";
-import { StatusIndicator } from "vue-status-indicator";
-import { functions } from "firebase";
-import { mapState, mapMutations } from "vuex";
-import fake from "@/helpers/fake";
-import ApplicantDropdown from "@/components/ApplicantDropdown.vue";
-import "vue-status-indicator/styles.css";
-import db from "../private/firebase_init";
+import Vue from 'vue';
+import { StatusIndicator } from 'vue-status-indicator';
+import { functions } from 'firebase';
+import { mapState, mapMutations } from 'vuex';
+import fake from '@/helpers/fake';
+import ApplicantDropdown from '@/components/ApplicantDropdown.vue';
+import 'vue-status-indicator/styles.css';
+import db from '../private/firebase_init';
 
 export default {
-  name: "DataTable",
-  methods: {
-    isMarked(app) {
-      console.log("IM", app.name, app);
-      app.decision.reviewers.forEach((val, ind) => {
-        console.log(
-          "VALUU",
-          val.reviewer,
-          this.$store.state.firebase.auth().currentUser.email
-        );
-        if (
-          val.reviewer === this.$store.state.firebase.auth().currentUser.email
-        ) {
-          console.log("NIBBA");
-          return true;
-        }
-      });
-      return false;
-    },
-    ...mapMutations(["update_DataTable_lastVisible"]),
-    f() {
-      console.log(fake);
-    },
-    async fb() {
-      db
-        .collection("applications")
-        .doc("DH5_Test")
-        .collection("all")
-        .get();
-    },
-    selectRow(e, props) {
-      // this is still kinda janky but seems to work
-      props.expanded = !props.expanded;
-      const offset = 50 * props.index;
-      window.scrollTo(0, window.screen.height / 2 + offset);
-    },
-    async nextPage() {
-      console.log("Page is: ", this.page);
-      if (!this.applications[`${this.page}`]) {
-        console.log("Getting next page");
-        const result = await db
-          .collection(this.collection)
-          .doc(this.hackathon)
-          .collection(this.bucket)
-          .orderBy("index")
-          .limit(this.rowsPerPage)
-          .startAfter((this.page - 1) * 20)
-          .get();
-        this.update_DataTable_lastVisible(result.docs[result.docs.length - 1]);
-        Vue.set(
-          this.applications,
-          this.page - 1,
-          result.docs.map(a => a.data())
-        );
-      }
-    },
-    async applicantCount() {
-      let size = 0;
-      await db
-        .collection(this.collection)
-        .doc(this.hackathon)
-        .collection(this.bucket)
-        .get()
-        .then(snap => {
-          size = 5;
-        });
-      return (size = 5);
-    }
-  },
-  components: {
-    ApplicantDropdown,
-    StatusIndicator
-  },
-  data() {
-    return {
-      // lastVisible: null,
-      page: 1,
-      rowsPerPage: 20,
-      numApplicants: 0,
-      applications: {},
-      peeps: [fake],
-      current: "All Applicants",
-      items: ["All Applicants", "Accepted Applicants", "Unaccepted Applicants"],
-      collection: "decisions",
-      hackathon: "DH5",
-      bucket: "pending",
-      buckets: [
-        {
-          title: "Pending Applications",
-          db_key: "in progre"
+    name: 'DataTable',
+    methods: {
+        isMarked(app) {
+            console.log('IM', app.name, app);
+            app.decision.reviewers.forEach((val, ind) => {
+                console.log(
+                    'VALUU',
+                    val.reviewer,
+                    this.$store.state.firebase.auth().currentUser.email
+                );
+                if (val.reviewer === this.$store.state.firebase.auth().currentUser.email) {
+                    console.log('NIBBA');
+                    return true;
+                }
+            });
+            return false;
         },
-        {
-          title: "Unsubmitted Applications",
-          db_key: "submitted"
+        ...mapMutations(['update_DataTable_lastVisible']),
+        f() {
+            console.log(fake);
         },
-        {
-          title: "Accepted Applicants",
-          db_key: "submitted"
+        async fb() {
+            db
+                .collection('applications')
+                .doc('DH5_Test')
+                .collection('all')
+                .get();
         },
-        {
-          title: "Rejected Applicants",
-          db_key: "submitted"
-        }
-      ],
-      search: "",
-      rating: null,
-      fake,
-      expanded: {},
-      headers: [
-        {
-          text: "Name",
-          align: "left",
-          sortable: false,
-          value: "name"
+        selectRow(e, props) {
+            // this is still kinda janky but seems to work
+            props.expanded = !props.expanded;
+            const offset = 50 * props.index;
+            window.scrollTo(0, window.screen.height / 2 + offset);
         },
-        { text: "Email", value: "email" },
-        { text: "University", value: "university" },
-        { text: "Applied (seconds)", value: "applied" },
-        { text: "Phone", value: "phone" },
-        { text: "Age", value: "age" },
-        { text: "Status", value: "rate" },
-        { text: "", value: "rate" }
-      ]
-    };
-  },
-  async mounted() {
-    const parent = this;
-    await db
-      .collection(this.collection)
-      .doc(this.hackathon)
-      .collection(this.bucket)
-      .get()
-      .then(snap => {
-        console.log(snap.size);
-        this.numApplicants = Math.ceil(snap.size / this.rowsPerPage);
-        console.log(this.numApplicants);
-      });
+        async nextPage() {
+            console.log('Page is: ', this.page);
+            if (!this.applications[`${this.page}`]) {
+                console.log('Getting next page');
+                const result = await db
+                    .collection(this.collection)
+                    .doc(this.hackathon)
+                    .collection(this.bucket)
+                    .orderBy('index')
+                    .limit(this.rowsPerPage)
+                    .startAfter((this.page - 1) * 20)
+                    .get();
+                this.update_DataTable_lastVisible(result.docs[result.docs.length - 1]);
+                Vue.set(this.applications, this.page - 1, result.docs.map(a => a.data()));
+            }
+        },
+        async applicantCount() {
+            let size = 0;
+            await db
+                .collection(this.collection)
+                .doc(this.hackathon)
+                .collection(this.bucket)
+                .get()
+                .then(snap => {
+                    size = 5;
+                });
+            return (size = 5);
+        },
+    },
+    components: {
+        ApplicantDropdown,
+        StatusIndicator,
+    },
+    data() {
+        return {
+            // lastVisible: null,
+            page: 1,
+            rowsPerPage: 20,
+            numApplicants: 0,
+            applications: {},
+            peeps: [fake],
+            current: 'All Applicants',
+            items: ['All Applicants', 'Accepted Applicants', 'Unaccepted Applicants'],
+            collection: 'decisions',
+            hackathon: 'DH5',
+            bucket: 'pending',
+            buckets: [
+                {
+                    title: 'Pending Applications',
+                    db_key: 'in progre',
+                },
+                {
+                    title: 'Unsubmitted Applications',
+                    db_key: 'submitted',
+                },
+                {
+                    title: 'Accepted Applicants',
+                    db_key: 'submitted',
+                },
+                {
+                    title: 'Rejected Applicants',
+                    db_key: 'submitted',
+                },
+            ],
+            search: '',
+            rating: null,
+            fake,
+            expanded: {},
+            headers: [
+                {
+                    text: 'Name',
+                    align: 'left',
+                    sortable: false,
+                    value: 'name',
+                },
+                { text: 'Email', value: 'email' },
+                { text: 'University', value: 'university' },
+                { text: 'Applied (seconds)', value: 'applied' },
+                { text: 'Phone', value: 'phone' },
+                { text: 'Age', value: 'age' },
+                { text: 'Status', value: 'rate' },
+                { text: '', value: 'rate' },
+            ],
+        };
+    },
+    async mounted() {
+        const parent = this;
+        await db
+            .collection(this.collection)
+            .doc(this.hackathon)
+            .collection(this.bucket)
+            .get()
+            .then(snap => {
+                console.log(snap.size);
+                this.numApplicants = Math.ceil(snap.size / this.rowsPerPage);
+                console.log('Number apps: ', this.numApplicants);
+            });
 
-    if (!this.applications[this.page - 1]) {
-      console.log("In mount fill");
-      const result = await db
-        .collection(this.collection)
-        .doc(this.hackathon)
-        .collection(this.bucket)
-        .orderBy("index")
-        .limit(this.rowsPerPage)
-        .get();
-      this.update_DataTable_lastVisible(result.docs[result.docs.length - 1]);
-      Vue.set(this.applications, this.page - 1, result.docs.map(a => a.data()));
-    }
-  },
-  computed: {
-    lastVisible: "DataTable.lastVisible"
-  }
+        if (!this.applications[this.page - 1]) {
+            console.log('In mount fill');
+            const result = await db
+                .collection(this.collection)
+                .doc(this.hackathon)
+                .collection(this.bucket)
+                .orderBy('index')
+                .limit(this.rowsPerPage)
+                .get();
+            this.update_DataTable_lastVisible(result.docs[result.docs.length - 1]);
+            Vue.set(this.applications, this.page - 1, result.docs.map(a => a.data()));
+        }
+    },
+    computed: {
+        lastVisible: 'DataTable.lastVisible',
+    },
 };
 </script>
 
 <style>
 .card_title {
-  font-weight: 500;
+    font-weight: 500;
 }
 
 #dropdown {
-  width: 100%;
+    width: 100%;
 }
 </style>
