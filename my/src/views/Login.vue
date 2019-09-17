651ceca4142c9f4dade1fcbcfbb4f3d6e9f468e8
Anant Jain <anant.j2409@gmail.com>
2019-09-17 07:51:23 -0400

    Trying something + image

--- my/src/views/Login.vue
+++ my/src/views/Login.vue
@@ -8,7 +8,7 @@
       <!-- "style="background-image: url('https://wallpapersite.com/images/pages/pic_w/14088.png');" -->
       <div class="wrap-login100">
         <div class="login100-pic">
-					<img src="@/assets/logo.png" alt="IMG">
+					<img src="@/assets/login_left.png" alt="IMG">
 				</div>
         <form class="login100-form validate-form">
           <!-- <span class="login100-form-logo">
