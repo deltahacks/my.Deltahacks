bee048fd100f68ff0ea28e70c94f0bacab4d0c91
Kumail <kumailmn@gmail.com>
2019-09-28 01:02:42 -0400

    clean up store

--- my/src/store.ts
+++ my/src/store.ts
@@ -1,72 +1,23 @@
 /* eslint-disable camelcase */
-import Vue from 'vue';
-import Vuex from 'vuex';
-import firebase from 'firebase';
-import db from './private/firebase_init';
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
+import db from "./private/firebase_init";
 
 // Use vuex with Vue
 Vue.use(Vuex);
 
 interface StoreState {
-  hackathon: string,
-  vuex_current_user: null,
-  vuex_msgs: [],
-  vuex_name: null,
-  user_name: string,
-  vuex_email: string,
-  vuex_password: string,
-  vuex_user_has_applied: boolean,
-  db,
-  firebase,
-  test: string,
-  vuex_user_application: null,
-  DataTable: {
-    lastVisible: null,
-  },
+  //TODO(typing) type these as firebase objects
+  db: any;
+  firebase: any;
 }
-
 
 // export the store
 export default new Vuex.Store({
   // Vuex state variables appended with vuex in front
   state: {
-    hackathon: 'DH6',
-    vuex_current_user: null,
-    vuex_msgs: [],
-    vuex_name: null,
-    user_name: 'Kumail',
-    vuex_email: null,
-    vuex_password: null,
-    vuex_user_has_applied: false,
     db,
     firebase,
-    test: 'test1',
-    vuex_user_application: null,
-    DataTable: {
-      lastVisible: null,
-    },
-  },
-  getters: {
-    get_vuex_email(state) {
-      return state.vuex_email;
-    },
-    get_vuex_user_application(state) {
-      return state.vuex_user_application;
-    },
-  },
-  // TODO: Implement Getters and Setters
-  mutations: {
-    update_vuex_email(state, email) {
-      state.vuex_email = email;
-    },
-    update_vuex_password(state, pass) {
-      state.vuex_password = pass;
-    },
-    update_vuex_current_user(state, usr) {
-      state.vuex_current_user = usr;
-    },
-    update_DataTable_lastVisible(state, change) {
-      state.DataTable.lastVisible = change;
-    },
   },
 });
