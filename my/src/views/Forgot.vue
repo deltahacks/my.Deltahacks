00acdcf631a00cf0821716e37e106d47a52cd181
Anant Jain <anant.j2409@gmail.com>
2019-09-29 22:35:51 -0400

    Stopped Images from Being Dragged by mouse

--- my/src/views/Forgot.vue
+++ my/src/views/Forgot.vue
@@ -13,13 +13,13 @@
       <!-- "style="background-image: url('https://wallpapersite.com/images/pages/pic_w/14088.png');" -->
       <div class="wrap-login100">
           <div class="card">
-          <img src="../assets/sidebar.png" alt="Sidebar" class="sidebar">
+          <img src="../assets/sidebar.png" draggable="false" alt="Sidebar+Logo" class="sidebar">
         </div>
         <form class="login100-form validate-form">
           <!-- <span class="login100-form-logo">
             <img src="@/assets/logo.png" height="90" width="90" alt="DeltaHacks Logo" />
           </span> -->
-          <img src="../assets/vi.png" class="back-vi"/>
+          <img src="../assets/vi.png" draggable="false" class="back-vi"/>
           <v-container>
             <v-layout>
             <v-flex xs12>
