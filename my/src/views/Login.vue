ea4c0190a32e08a749b05b345e418378e733433a
Anant Jain <anant.j2409@gmail.com>
2019-10-05 00:37:19 -0400

    Adding Animations

--- my/src/views/Login.vue
+++ my/src/views/Login.vue
@@ -18,8 +18,8 @@
         <!-- <span class="login100-form-logo">
             <img src="@/assets/logo.png" height="90" width="90" alt="DeltaHacks Logo" />
         </span>-->
-        <template v-if="counter === 0">
         <img src="../assets/vi.png" draggable="false" class="back-vi" />
+        <template v-if="counter === 0">
         <div class="spanText">
           <span class="txt1">my</span>
           <span class="txt2">Delta</span>
@@ -67,8 +67,7 @@
           </div>
         </div>
         </template>
-        <template v-else-if="counter === 1">
-             <img src="../assets/vi.png" draggable="false" class="back-vi"/>
+                     <template v-else-if="counter === 1">
             <div class="spanText">
             <span class="txt1">
               Forgot </span><span class="txt2">Pass</span><span class="txt3">word
@@ -89,7 +88,7 @@
             Submit
         </button>
           </div>
-                 <div class="backdiv">
+                 <div class="forgotdiv">
                    <br>
             <a class="forgot"  v-on:click="counter=0">
                 <i class="fas fa-arrow-left" /> Go Back
