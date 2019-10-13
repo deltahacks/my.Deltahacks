1ac5a071d76b59109bad069cd4013698b917243c
Anant Jain <anant.j2409@gmail.com>
2019-10-13 17:26:57 -0400

    Hello Page Creation

---
+++ my/src/views/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+      integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+      crossorigin="anonymous"
+    />
+  </head>
+  <div class="container-login100 background">
+    <!-- "style="background-image: url('https://wallpapersite.com/images/pages/pic_w/14088.png');" -->
+      <h1 class="hellotext">Hello</h1>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Login',
+  mounted() {},
+  props: {
+    source: String,
+  },
+  watch: {
+    loader() {
+      const l: any = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => {
+        this[l] = false;
+      }, 3000);
+      this.loader = null;
+    },
+  },
+});
+</script>
+<style scoped src='../assets/css/hello.css'>
+</style>
