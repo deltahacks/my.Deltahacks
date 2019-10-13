898e34caed3cf0c0edff707359c96da72e01ae49
amirafzali <amir@afzali.ca>
2019-10-13 19:20:13 -0400

    Remove console log

--- my/src/firebase_init.ts
+++ my/src/firebase_init.ts
@@ -2,7 +2,6 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Initialize Firebase
-console.log(process.env);
 const config = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
