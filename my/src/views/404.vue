c7df58633f27bc688910cd55073ffeca4ca3750c
Anant Jain <anant.j2409@gmail.com>
2019-10-16 19:44:14 -0400

    Updated 404

--- my/src/views/404.vue
+++ my/src/views/404.vue
@@ -8,7 +8,7 @@
       </div>
     <span class="txt3">404 :(</span><br>
         <span class="txt4">Sorry, we were not able to find the page you were looking for.</span><br>
-        <a href="https://www.deltahacks.com" class="txt5">Go back to main site.</a>
+        <a href="https://www.my.deltahacks.com" class="txt5">Go back to main site.</a>
   <img src="../assets/vi2.png" draggable="false" class="back-vi" />
     </div>
   </div>
