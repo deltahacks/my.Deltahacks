89b7b4b901c0a1fc87a37276e5ed6fbcd9d335c4
Anant Jain <anant.j2409@gmail.com>
2019-10-15 21:21:39 -0400

    404 Page Update

--- my/src/views/404.vue
+++ my/src/views/404.vue
@@ -1,34 +1,16 @@
 <template>
-  <html>
-  <div class="stars"></div>
-  <div class="twinkling"></div>
-  <div class="container-error100">
-    <section class="error-container">
-      <div class="topnav">
-        <a href="/dashboard">Applicant Dashboard</a>
-        <a href="/">Logout</a>
-        <a href="/apply">Apply</a>
-        <a href="/status">Status</a>
-        <h1>Error 404</h1>
+  <div class="container-error100 background">
+    <div class="textitems">
+     <div class="spanText">
+            <span class="txt2">Delta</span>
+            <span class="txt1">Hacks</span>
+            <span class="txt2"> VI</span>
       </div>
-      <span>4</span>
-      <span>0</span>
-      <span>4</span>
-      <section class="wrap">
-        <p>Page Not Found
-          <p>
-            <section class="wrap2">
-              <a href="javascript:history.back()">Go Back To The Previous Page</a>
-            </section>
-            <section class="wrap2">
-              <a href="/">Go Home</a>
-            </section>
-            <h2><br>DeltaHacks VI</h2>
-      </section>
-    </section>
+    <span class="txt3">404 :(</span><br>
+        <span class="txt4">Sorry, we were not able to find the page you were looking for.</span><br>
+        <a href="https://www.deltahacks.com" class="txt5">Go back to main site.</a>
+    </div>
   </div>
-
-  </html>
 </template>
 
 <script>
