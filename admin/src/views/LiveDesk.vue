a6344c2c701bc1c2b0c6abfe03930e6ff6519bbb
dhruv423 <dhruvbhavsar25@hotmail.com>
2020-10-18 14:06:53 -0400

    Remove from computed and make a variable outside

--- admin/src/views/LiveDesk.vue
+++ admin/src/views/LiveDesk.vue
@@ -160,6 +160,8 @@ import Vue from 'vue';
 import db from '../firebase_init';
 import Navbar from '@/components/Navbar.vue';
 
+const currentHackathon = 'DH7';
+
 export default Vue.extend({
   name: 'LiveDesk',
   components: {
@@ -204,9 +206,6 @@ export default Vue.extend({
     fullName(): string {
       return `${this.application.name.first} ${this.application.name.last}`;
     },
-    currentHackathon(): string {
-      return 'DH7'
-    }
   },
   methods: {
     // saveDownLoadLinks() {
@@ -229,7 +228,7 @@ export default Vue.extend({
     async reset() {
       const admin = firebase.auth().currentUser!.email;
       const ref = db
-        .collection(this.currentHackathon)
+        .collection(currentHackathon)
         .doc('hackathon')
         .collection('live desk')
         .doc(admin || undefined);
@@ -261,13 +260,13 @@ export default Vue.extend({
         this.rejectRegistration();
         return;
       }
-      app.type = target.slice(0,-1);
+      app.type = target.slice(0, -1);
       // parse name field
       // const [first, last] = app.name.split(' ');
       // app.name = first;
       // app.lastname = last || '';
       // add to respective directory
-      db.collection(this.currentHackathon)
+      db.collection(currentHackathon)
         .doc('hackathon')
         .collection(target)
         .doc(app.contact.email)
@@ -289,7 +288,7 @@ export default Vue.extend({
         if (email.length === 0) resolve({ found: false, data: {} });
 
         const snap = await db
-          .collection(this.currentHackathon)
+          .collection(currentHackathon)
           .doc('applications')
           .collection('all')
           .doc(email)
@@ -354,7 +353,7 @@ export default Vue.extend({
       app.type = type;
       try {
         await db
-          .collection(this.currentHackathon)
+          .collection(currentHackathon)
           .doc('hackathon')
           .collection('checked in')
           .doc(this.application.contact.email)
@@ -385,7 +384,7 @@ export default Vue.extend({
       );
       const imageOffset = (badge.internal.pageSize.width - 25) / 2;
       badge.addImage(QRImage, 'JPEG', imageOffset, 3, 25, 25);
-      badge.save(`${this.currentHackathon}_${this.application.name.first}${this.application.name.last}`);
+      badge.save(`${currentHackathon}_${this.application.name.first}${this.application.name.last}`);
     },
     // should insert / generate the back of DH7 badge.
     async createTemplate() {
@@ -398,7 +397,7 @@ export default Vue.extend({
         t.text(textOffset, y, text);
       };
       const snap = await db
-        .collection(this.currentHackathon)
+        .collection(currentHackathon)
         .doc('hackathon')
         .collection('checked in')
         .doc(this.application.contact.email)
@@ -445,7 +444,7 @@ export default Vue.extend({
   async beforeMount() {
     const admin = firebase.auth().currentUser!.email;
     const ref = db
-      .collection(this.currentHackathon)
+      .collection(currentHackathon)
       .doc('hackathon')
       .collection('live desk')
       .doc(admin || undefined);
